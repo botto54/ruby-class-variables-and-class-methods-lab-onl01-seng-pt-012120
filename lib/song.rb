f7ed47eeff5efbb55c687b22f1e29b295e97f27a
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-03-01 01:24:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   
   def self.genre_count
     @@genre_count = {}
-    @@genres.group_by(&:itself).each {|h,k| @@genre_count[h] = v.count}
+    @@genres.group_by(&:itself).each {|h,v| @@genre_count[h] = v.count}
   end
   
   def self.artist_count
